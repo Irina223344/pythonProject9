4e7ff471e8e99241e658e738050c0eecf59acfb0
Irina223344 <Lyusi223344@gmail.com>
2022-11-28 09:34:22 +0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+array = [int(x) for x in input("Введите числа от 1 до 999 в любом порядке, через пробел: ").split()]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def merge_sort(array):  # "разделяй"
+    if len(array) < 2:  # если кусок массива равен 2,
+        return array[:]  # выход из рекурсии
+    else:
+        middle = len(array) // 2  # ищем середину
+        left = merge_sort(array[:middle])  # рекурсивно делим левую часть
+        right = merge_sort(array[middle:])  # и правую
+        return merge(left, right)  # выполняем слияние
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+print(merge_sort(array))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def binary_search(array, element, left, right):
+    if left > right:  # если левая граница превысила правую,
+        return False  # значит элемент отсутствует
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    middle = (right + left) // 2  # находимо середину
+    if array[middle] == element:  # если элемент в середине,
+        return middle  # возвращаем этот индекс
+    elif element < array[middle]:  # если элемент меньше элемента в середине
+        # рекурсивно ищем в левой половине
+        return binary_search(array, element, left, middle - 1)
+    else:  # иначе в правой
+        return binary_search(array, element, middle + 1, right)
+
+
+while True:
+    try:
+        element = int(input("Введите число от 1 до 999: "))
+        if element < 0 or element > 999:
+            raise Exception
+        break
+    except ValueError:
+        print("Нужно ввести число!")
+    except Exception:
+        print("Неправильный диапазон!")
+
+
+print(binary_search(array, element, 0,  len(array)))
